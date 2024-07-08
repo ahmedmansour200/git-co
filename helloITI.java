9adff51b140156afb1bd44c112bf59bcf3f4d414
ahmed <ahmedmansour10122000@gmail.com>
2024-07-08 13:46:30 +0300

    add print hello git in class hello iti

--- helloITI.java
+++ helloITI.java
@@ -1,5 +1,6 @@
     public class PrintITI {
         public static void main(String[] args) {
             System.out.println("Hello ITI");
+            System.out.println("Hello Git");
         }
     }
